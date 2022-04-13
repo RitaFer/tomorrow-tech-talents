e0c9d2e3679c53f8f10472ea7a0cdafb471933ea
RitaFer <rialf.ferreira@gmail.com>
2022-04-13 20:53:45 -0300

    resolve ex01

--- src/aula03/exercicios/ex01.java
+++ src/aula03/exercicios/ex01.java
@@ -6,5 +6,25 @@ se o aluno foi aprovado caso apresente o valor da média escolar maior ou igual 
 apresente uma mensagem informando que ele está de recuperação.
  */
 
+import java.util.Scanner;
+
 public class ex01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double[] grades = new Double[4];
+        double sum = 0;
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Insira a " + (i + 1) + "ª nota do aluno: ");
+            grades[i] = scanner.nextDouble();
+            sum += grades[i];
+        }
+
+        double average = sum / 4;
+
+        if (average >= 7){
+            System.out.println("\nAPROVADO!!");
+        } else {
+            System.out.println("\nDe recuperação!!");
+        }
+    }
 }
