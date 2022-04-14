fd8d7660823a3b7af360e3afadb84347e3cf0222
RitaFer <rialf.ferreira@gmail.com>
2022-04-13 21:21:07 -0300

    resolve ex04

--- src/aula03/exercicios/ex04.java
+++ src/aula03/exercicios/ex04.java
@@ -5,5 +5,32 @@ package aula03.exercicios;
 e imprima na tela os valores em ordem decrescente.
  */
 
+import java.util.Scanner;
+
 public class ex04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double[] numbers = new Double[5];
+        for (int i = 0; i < numbers.length; i++){
+            System.out.print("Insira o " + (i+1) + "º número:");
+            numbers[i] = scanner.nextDouble();
+        }
+
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] > numbers[j]) {
+                    double aux = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = aux;
+                }
+
+            }
+        }
+
+        System.out.println("\nOs números inseridos em forma decrescente: ");
+        for (double n : numbers) {
+            System.out.println("- " + n + ";");
+        }
+    }
+
 }
