1fa70a4d6980e6e684430208512aa9cadd3d9c5c
RitaFer <rialf.ferreira@gmail.com>
2022-04-13 21:03:13 -0300

    resolve ex02

--- src/aula03/exercicios/ex02.java
+++ src/aula03/exercicios/ex02.java
@@ -5,5 +5,23 @@ package aula03.exercicios;
 ou negativo. Pare a execução do programa quando o usuário requisitar (O programa deve parar quando o usuário escolher 0).
  */
 
+import java.util.Scanner;
+
 public class ex02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number;
+        do {
+            System.out.print("Insira um número qualquer, ou insira 0 para parar a aplicação:");
+            number = scanner.nextDouble();
+            if (number > 0) {
+                System.out.println("Você inseriu um número positivo !");
+            } else if(number < 0) {
+                System.out.println("Você inseriu um número negativo !");
+            } else {
+                System.out.println("\nTCHAUUUUUUU !!!!");
+                System.exit(0);
+            }
+        } while (number != 0);
+    }
 }
