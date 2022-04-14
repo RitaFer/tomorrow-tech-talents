aff72a91fe9a447a32e3fa3891b40f8173835a76
RitaFer <rialf.ferreira@gmail.com>
2022-04-13 23:07:29 -0300

    resolve ex07

--- src/aula03/exercicios/ex07.java
+++ src/aula03/exercicios/ex07.java
@@ -7,5 +7,20 @@ Explicação: 6 x 5 x 4 x 3 x 2 x 1 = 720
 
  */
 
+import java.util.Scanner;
+
 public class ex07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number, fatorial = 1;
+
+        System.out.println("Digite um nº");
+        number = scanner.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            fatorial = fatorial * i;
+        }
+
+        System.out.println("!" + number + " = " + fatorial);
+    }
 }
