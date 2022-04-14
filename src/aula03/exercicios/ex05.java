73f38e44c0197ad6b2b1736bcb6dffad8d842a58
RitaFer <rialf.ferreira@gmail.com>
2022-04-13 21:48:24 -0300

    resolve ex05

--- src/aula03/exercicios/ex05.java
+++ src/aula03/exercicios/ex05.java
@@ -8,5 +8,25 @@ está: abaixo do peso (IMC inferior a 18,5), com peso normal (IMC entre 18,5 a 2
 Use a fórmula: IMC = Peso ÷ (Altura × Altura)
  */
 
+import java.util.Scanner;
+
 public class ex05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira a sua altura, em metros: (ex: 1,62) ");
+        double height = scanner.nextDouble();
+        System.out.print("Insira seu peso, em kg: (ex: ) ");
+        double weight = scanner.nextDouble();
+
+        double imc = weight / (height * height);
+
+        if (imc < 18.5) {
+            System.out.print("Magrão!! Você está abaixo do peso, pode comer mais!!");
+        } else if (imc >= 18.5 && imc <= 24.9){
+            System.out.print("Uia !! Tá fitness ein?!!");
+        } else {
+            System.out.print("Ixe, seria bom uma dietinha!!");
+        }
+    }
+
 }
