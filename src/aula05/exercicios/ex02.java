2eb13db218ca794acb171e750a1da9c9aee120b0
RitaFer <rialf.ferreira@gmail.com>
2022-04-25 21:46:51 -0300

    resolve ex02

--- src/aula05/exercicios/ex02.java
+++ src/aula05/exercicios/ex02.java
@@ -10,11 +10,42 @@ e a quantidade de horas trabalhadas no mês.
 
 a. Desconto do IR;
 b. Salário Bruto até R$900,00 (inclusive) – Isento;
-c. Salário Bruto de R$ 1500, 00 (inclusive) – desconto de 5%;
+c. Salário Bruto de R$ 1500,00 (inclusive) – desconto de 5%;
 d. Salário bruto até R$ 2500,00 (Inclusive) – desconto de 10%;
-e. Salário bruto acima de 2500 – Desconto de 20%.
+e. Salário bruto acima de R$2500,00 – Desconto de 20%.
 
  */
 
+import java.util.Scanner;
+
 public class ex02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Qual o valor da sua hora? R$");
+        double wageHour = scanner.nextDouble();
+        System.out.println("Quantas horas você trabalhou no último mês? ");
+        int hours = scanner.nextInt();
+
+        double grossSalary = wageHour * hours;
+        double fgts = (grossSalary * 11)/100;
+        double tradeUnion = (grossSalary * 3)/100;
+        double irpf = 0;
+
+        if(grossSalary > 900 && grossSalary <= 1500){
+            irpf = (grossSalary * 5)/100;
+        } else if(grossSalary > 1500 && grossSalary <= 2500){
+            irpf = (grossSalary * 10)/100;
+        } else{
+            irpf = (grossSalary * 20)/100;
+        }
+
+        System.out.println("=====================================================");
+        System.out.println("Salário Bruto: R$ " + grossSalary + " (+)");
+        System.out.println("IRPF: R$ " + irpf + " (-)");
+        System.out.println("Contribuição Sindical: R$ " + tradeUnion + " (-)");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Salário Líquido: R$ " + (grossSalary - tradeUnion - irpf));
+        System.out.println("FGTS: R$ " + fgts);
+        System.out.println("=====================================================");
+    }
 }
