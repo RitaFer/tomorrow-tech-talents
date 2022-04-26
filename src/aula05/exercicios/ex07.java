946bc8b5444d30840837293e0ed6078cc429b58f
RitaFer <rialf.ferreira@gmail.com>
2022-04-25 22:59:07 -0300

    resolve ex07

--- src/aula05/exercicios/ex07.java
+++ src/aula05/exercicios/ex07.java
@@ -9,5 +9,36 @@ O valor total das compras efetuadas
 O valor da primeira prestação das compras a prazo, sabendo-se que essas serão pagas em três vezes
  */
 
+import java.util.Scanner;
+
 public class ex07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double totalValue = 0;
+        double totalInCash = 0;
+        double totalInstallments = 0;
+        double parcel = 0;
+        double value = 0;
+
+        for(int i = 0; i < 15; i++){
+            System.out.println("Qual o valor da transação? ");
+            value = scanner.nextDouble();
+            System.out.println("Qual a forma de pagamento? ");
+            String payment = scanner.nextLine();
+            char pay = payment.toUpperCase().charAt(0);
+            totalValue += value;
+
+            if(pay == 'V') {
+                totalInCash += value;
+            } else if (pay == 'P'){
+                totalInstallments += value;
+                parcel = value / 3;
+                System.out.println("O valor da primeira prestação da compra nº: " + (i+1) + ", paga a prazo é R$ " + parcel);
+            }
+        }
+
+        System.out.println("O valor total das compras à vista: R$ " + totalInCash);
+        System.out.println("O valor total das compras a prazo: R$ " + totalInstallments);
+        System.out.println("O valor total das compras efetuadas: R$ " + totalValue);
+    }
 }
