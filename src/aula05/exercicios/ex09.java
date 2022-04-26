3d1e8dec7dd9c42b11af8da966d42f0925d98bdc
RitaFer <rialf.ferreira@gmail.com>
2022-04-26 11:45:07 -0300

    resolve ex09

--- src/aula05/exercicios/ex09.java
+++ src/aula05/exercicios/ex09.java
@@ -11,5 +11,43 @@ Digite a opção desejada:
 Verifique a possibilidade de opção inválida, mostrando uma mensagem.
  */
 
+import java.util.Scanner;
+
 public class ex09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=============================================================\n");
+        System.out.println("Escolha uma opção de cálculo:");
+        System.out.println("1. Média Aritmética");
+        System.out.println("2. Média Ponderada");
+        System.out.println("\n=============================================================");
+        int option = scanner.nextInt();
+
+        while (option != 1 && option != 2) {
+            System.out.println("Opção inválida, por favor, escolha 1 ou 2:");
+            option = scanner.nextInt();
+        }
+
+        if(option == 1){
+            System.out.print("Insira a primeira nota: ");
+            double firstNote = scanner.nextDouble();
+            System.out.print("Insira a segunda nota: ");
+            double secondNote = scanner.nextDouble();
+
+            System.out.println("\nA média aritmética dos valores " + firstNote + " e " + secondNote + " é: " + ((firstNote + secondNote)/2));
+        } else {
+            double sumNotes = 0, sumWeight = 0;
+            for (int i = 0; i < 3; i++){
+                System.out.print("Insira a " + (i+1) + "ª nota: ");
+                double note = scanner.nextDouble();
+                System.out.print("Insira o peso da " + (i+1) + "ª nota: ");
+                double weight = scanner.nextDouble();
+
+                sumNotes += (note*weight);
+                sumWeight += weight;
+            }
+
+            System.out.println("\bA média ponderada dos valores inseridos é: " + sumNotes / sumWeight);
+        }
+    }
 }
