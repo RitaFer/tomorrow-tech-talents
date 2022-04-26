5bfbd8b4bf7c33f30d32d09180bdd46245042f4f
RitaFer <rialf.ferreira@gmail.com>
2022-04-26 11:13:18 -0300

    resolve ex08

--- src/aula05/exercicios/ex08.java
+++ src/aula05/exercicios/ex08.java
@@ -6,5 +6,29 @@ A quantidade de pessoas com mais de 90 quilos;
 A média das idades das sete pessoas;
  */
 
+import java.util.Scanner;
+
 public class ex08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age, moreThanNinety = 0, sumAge = 0;
+        double weight;
+
+        for(int i = 0; i < 7; i++) {
+            System.out.print("Qual a idade da " + (i + 1) + "ª pessoa? ");
+            age = scanner.nextInt();
+            System.out.print("Qual o peso, em kg, da " + (i + 1) + "ª pessoa? ");
+            weight = scanner.nextDouble();
+
+            if (weight > 90) {
+                moreThanNinety++;
+            }
+
+            sumAge += age;
+        }
+        System.out.println("=============================================================\n");
+        System.out.println("A quantidade de pessoas com mais de 90kg é " + moreThanNinety);
+        System.out.println("A média de idades é " + (sumAge / 7));
+        System.out.println("\n=============================================================");
+    }
 }
