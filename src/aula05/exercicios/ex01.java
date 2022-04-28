855fbaa6c505279943e6123e3186f3ef88d7bd56
RitaFer <rialf.ferreira@gmail.com>
2022-04-28 00:13:30 -0300

    Update ex01.java

--- src/aula05/exercicios/ex01.java
+++ src/aula05/exercicios/ex01.java
@@ -10,15 +10,15 @@ import java.util.Scanner;
 public class ex01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        System.out.println("Há quantos anos você fuma? ");
-        int years = scanner.nextInt();
-        System.out.println("Quantos cigarros por dia você consome? ");
+        System.out.print("Há quantos anos você fuma? ");
+        int daysOfUse = scanner.nextInt()*365;
+        System.out.print("Quantos cigarros por dia você consome? ");
         int cigarettesDay = scanner.nextInt();
         System.out.print("Insira o preço do maço, com 20 cigarros, R$ ");
-        double value = scanner.nextDouble();
+        double valuePerCigarette = scanner.nextDouble()/20;
 
-        double totalValue = (years * 365) * cigarettesDay * (value / 20);
+        double totalValue = daysOfUse * cigarettesDay * valuePerCigarette;
 
-        System.out.println("Você já gastou, aproximadamente, R$ " + totalValue + ", com cigarros.");
+        System.out.print("\nVocê já gastou, aproximadamente, R$ " + totalValue + ", com cigarros.");
     }
 }
