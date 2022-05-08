80f5f47c807f4bf20b4913a2813f2e4345529b20
RitaFer <rialf.ferreira@gmail.com>
2022-05-07 22:51:00 -0300

    Create ex01.java

---
+++ src/extras/cursoemvideo/ex01.java
@@ -0,0 +1,13 @@
+package extras.cursoemvideo;
+
+public class ex01 {
+    public static void main(String[] args) {
+        int x, y;
+        double z;
+
+        x = 5;
+        y = 2;
+        z = x / y;
+        System.out.print(z);
+    }
+}
