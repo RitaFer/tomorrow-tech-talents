1ffd1a2d0e87552162434720824c22420583b8c0
RitaFer <rialf.ferreira@gmail.com>
2022-05-11 21:09:59 -0300

    Create Teacher.java

---
+++ src/oop/school/Teacher.java
@@ -0,0 +1,41 @@
+package oop.school;
+
+import java.util.ArrayList;
+
+public class Teacher extends Employee {
+
+    private ArrayList<String> classes;
+    private ArrayList<Subject> subjects;
+
+    public Teacher(String name, String cpf, String rg, double salary, ArrayList<String> classes, ArrayList<Subject> subjects) {
+        super(name, cpf, rg, "Professor", salary);
+        this.classes = classes;
+        this.subjects = subjects;
+    }
+
+    public ArrayList<String> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(ArrayList<String> classes) {
+        this.classes = classes;
+    }
+
+    public ArrayList<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(ArrayList<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNome do Professor: "+this.getName()
+                +"\n CPF do Professor: "+this.getCpf()
+                +"\n RG do Professor: "+this.getRg()
+                +"\n Classes do Professor: "+this.getClass()
+                +"\n Disciplinas do Professor: R$ "+this.getSubjects()
+                +"\n Salário do Professor: R$ "+this.getSalary();
+    }
+}
