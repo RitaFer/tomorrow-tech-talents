d6328b3f29d333edb60fab3c8c9981ac170327ce
RitaFer <rialf.ferreira@gmail.com>
2022-05-11 21:10:08 -0300

    Create Subject.java

---
+++ src/oop/school/Subject.java
@@ -0,0 +1,27 @@
+package oop.school;
+
+public class Subject {
+    private String name;
+    private double note;
+
+    public Subject(String name){
+        this.name = name;
+        this.note = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getNote() {
+        return note;
+    }
+
+    public void setNote(double note) {
+        this.note = note;
+    }
+}
