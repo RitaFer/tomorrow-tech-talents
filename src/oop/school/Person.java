4f05b73345d69647d52184a20daab3c061cb6367
RitaFer <rialf.ferreira@gmail.com>
2022-05-11 21:10:35 -0300

    Create Person.java

---
+++ src/oop/school/Person.java
@@ -0,0 +1,37 @@
+package oop.school;
+
+public class Person {
+    private String name;
+    private String rg;
+    private String cpf;
+
+    public Person(String name, String cpf, String rg) {
+        this.name = name;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
