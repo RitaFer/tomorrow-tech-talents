53427227050f6e2243dbe97da9413751f2a2efa2
RitaFer <rialf.ferreira@gmail.com>
2022-05-11 21:10:32 -0300

    Create Employee.java

---
+++ src/oop/school/Employee.java
@@ -0,0 +1,35 @@
+package oop.school;
+
+public class Employee extends Person{
+
+    private String office;
+    private double salary;
+
+    protected Employee(String name, String cpf, String rg, String office, double salary) {
+        super(name, cpf, rg);
+        this.office = office;
+        this.salary = salary;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+    public void setOffice(String office) {
+        this.office = office;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNome do Funcionário: "+this.getName()
+                +"\n CPF do Funcionário: "+this.getCpf()
+                +"\n RG do Funcionário: "+this.getRg()
+                +"\n Cargo do Funcionário: "+this.getOffice()
+                +"\n Salário do Funcionário: R$ "+this.getSalary();
+    }
+}
