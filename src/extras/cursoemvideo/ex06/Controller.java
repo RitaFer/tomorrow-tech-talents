313a014c9288d4d664e7eecc9e6f0cfd87a6f6fd
RitaFer <rialf.ferreira@gmail.com>
2022-05-20 11:45:32 -0300

    Update Controller.java

--- src/extras/cursoemvideo/ex06/Controller.java
+++ src/extras/cursoemvideo/ex06/Controller.java
@@ -1,14 +1,14 @@
 package extras.cursoemvideo.ex06;
 
 public interface Controller {
-    public abstract void turnOn();
-    public abstract void turnOff();
-    public abstract void openMenu();
-    public abstract void closeMenu();
-    public abstract void moreSound();
-    public abstract void lessSound();
-    public abstract void withoutSound();
-    public abstract void withSound();
-    public abstract void play();
-    public abstract void pause();
+    void turnOn();
+    void turnOff();
+    void openMenu();
+    void closeMenu();
+    void moreSound();
+    void lessSound();
+    void withoutSound();
+    void withSound();
+    void play();
+    void pause();
 }
