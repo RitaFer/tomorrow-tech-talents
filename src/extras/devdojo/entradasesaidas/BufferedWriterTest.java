b451fa951b7f533f9d5b67ffe6c3969a96658951
RitaFer <rialf.ferreira@gmail.com>
2022-06-30 13:04:14 -0300

    Create BufferedWriterTest.java

---
+++ src/extras/devdojo/entradasesaidas/BufferedWriterTest.java
@@ -0,0 +1,21 @@
+package extras.devdojo.entradasesaidas;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterTest {
+    public static void main(String[] args) throws IOException {
+        File arch = new File("testFile.txt");
+        FileWriter archWriter = new FileWriter(arch);
+        try(BufferedWriter bf = new BufferedWriter(archWriter)){
+            bf.write("Isso é apenas um teste !");
+            bf.newLine();
+            bf.write("Isso é apenas um teste para ver se vai concatenar !\n");
+            bf.flush();
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
