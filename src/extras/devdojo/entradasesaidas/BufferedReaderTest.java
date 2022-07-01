4818532924b5ac4150855159926987d609af5815
RitaFer <rialf.ferreira@gmail.com>
2022-07-01 17:13:31 -0300

    Update BufferedReaderTest.java

--- src/extras/devdojo/entradasesaidas/BufferedReaderTest.java
+++ src/extras/devdojo/entradasesaidas/BufferedReaderTest.java
@@ -1,2 +1,21 @@
-package extras.devdojo.entradasesaidas;public class BufferedReaderTest {
+package extras.devdojo.entradasesaidas;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderTest {
+    public static void main(String[] args) throws IOException {
+        File arch = new File("testBuffered.txt");
+        FileReader archReader = new FileReader(arch);
+        try(BufferedReader br = new BufferedReader(archReader)){
+            String line;
+            while ((line=br.readLine())!=null){
+                System.out.print(line);
+            }
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
 }
